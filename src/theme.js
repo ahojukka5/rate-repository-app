75d2630f131cc203e958a07588f45491263c07f7
Jukka Aho <ahojukka5@gmail.com>
2020-12-14 05:20:52 +0700

    Add theme.js

---
+++ src/theme.js
@@ -0,0 +1,24 @@
+const theme = {
+  colors: {
+    textPrimary: '#24292e',
+    textSecondary: '#586069',
+    primary: '#0366d6',
+    dark: '#222222',
+    light: '#eeeeee',
+    white: '#ffffff'
+  },
+  fontSizes: {
+    body: 14,
+    subheading: 16,
+    heading: 20,
+  },
+  fonts: {
+    main: 'System',
+  },
+  fontWeights: {
+    normal: '400',
+    bold: '700',
+  },
+};
+
+export default theme;
