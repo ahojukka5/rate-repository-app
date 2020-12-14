4fb1da468be0a34927791e1a6424dc32b2389ef4
Jukka Aho <ahojukka5@gmail.com>
2020-12-14 18:24:39 +0700

    Create card layout using FlexBox

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,16 +1,56 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 12,
+    display: 'flex',
+  },
+  cardTop: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  cardBottom: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+});
 
 const RepositoryItem = ({ item }) => {
   return (
-    <View>
-      <Text>Full name: {item.fullName}</Text>
-      <Text>Description: {item.description}</Text>
-      <Text>Language: {item.language}</Text>
-      <Text>Stars: {item.stargazersCount}</Text>
-      <Text>Forks: {item.forksCount}</Text>
-      <Text>Reviews: {item.reviewCount}</Text>
-      <Text>Rating: {item.ratingAverage}</Text>
+    <View style={styles.container}>
+      <View style={styles.cardTop}>
+        <Image style={styles.tinyLogo} source={{ uri: item.ownerAvatarUrl }} />
+        <View>
+          <Text>{item.fullName}</Text>
+          <Text>{item.description}</Text>
+          <Text>{item.language}</Text>
+        </View>
+      </View>
+      <View style={styles.cardBottom}>
+        <View>
+          <Text>{item.stargazersCount}</Text>
+          <Text>Stars</Text>
+        </View>
+        <View>
+          <Text>{item.forksCount}</Text>
+          <Text>Forks</Text>
+        </View>
+        <View>
+          <Text>{item.reviewCount}</Text>
+          <Text>Reviews</Text>
+        </View>
+        <View>
+          <Text>{item.ratingAverage}</Text>
+          <Text>Rating</Text>
+        </View>
+      </View>
     </View>
   );
 };
