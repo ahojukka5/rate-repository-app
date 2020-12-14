e01299faca632e7f55d4b31f8fef1bf806cc6db8
Jukka Aho <ahojukka5@gmail.com>
2020-12-15 03:49:12 +0700

    Create pure SignIn view

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { TouchableWithoutFeedback, StyleSheet, View } from 'react-native';
 
+import theme from '../theme';
 import Text from './Text';
+import TextInput from './TextInput';
 
-const SignIn = () => {
-  return <Text>The sign in view</Text>;
+const styles = StyleSheet.create({
+  login: {
+    paddingTop: 30,
+  },
+  button: {
+    padding: 10,
+    paddingLeft: 20,
+    margin: 10,
+    marginBottom: 3,
+    backgroundColor: theme.colors.primary,
+    borderColor: 'black',
+    borderRadius: 7,
+    borderWidth: 1,
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+const SignIn = ({ onSubmit }) => {
+  return (
+    <View style={styles.login}>
+      <TextInput name="username" placeholder="Username" />
+      <TextInput name="password" placeholder="Password" secureTextEntry />
+      <TouchableWithoutFeedback onPress={onSubmit}>
+        <Text style={styles.button}>Sign in</Text>
+      </TouchableWithoutFeedback>
+    </View>
+  );
 };
 
 export default SignIn;
