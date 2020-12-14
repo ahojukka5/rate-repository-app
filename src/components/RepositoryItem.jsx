51d04d10a36433d36c43ab974b0bf63d84e12ca2
Jukka Aho <ahojukka5@gmail.com>
2020-12-14 19:25:04 +0700

    Use Statistic to show statistics

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -2,54 +2,79 @@ import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
+  // Main container
   container: {
-    padding: 12,
+    padding: 18,
     display: 'flex',
+    flexDirection: 'column',
+    // backgroundColor: 'green',
   },
+
+  // Top part of card, having avatar and information
   cardTop: {
     display: 'flex',
     flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'green',
   },
+  tinyLogo: {
+    marginRight: 10,
+    flexGrow: 0,
+    width: 50,
+    height: 50,
+  },
+  infoBlock: {
+    flexGrow: 1,
+    flexShrink: 1,
+  },
+
+  // Bottom part of card, having statistics
   cardBottom: {
+    padding: 10,
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-around',
+    backgroundColor: 'yellow',
   },
-  tinyLogo: {
-    width: 50,
-    height: 50,
+
+  statisticsBlock: {
+    display: 'flex',
+    alignItems: 'center',
   },
 });
+
+const Statistic = ({ count, what }) => {
+  const formatNumber = (number) => {
+    if (number < 1000) {
+      return number;
+    }
+    return (number / 1000).toFixed(1) + 'k';
+  };
+  return (
+    <View style={styles.statisticsBlock}>
+      <Text>{formatNumber(count)}</Text>
+      <Text>{what}</Text>
+    </View>
+  );
+};
 
 const RepositoryItem = ({ item }) => {
   return (
     <View style={styles.container}>
       <View style={styles.cardTop}>
         <Image style={styles.tinyLogo} source={{ uri: item.ownerAvatarUrl }} />
-        <View>
+        <View style={styles.infoBlock}>
           <Text>{item.fullName}</Text>
           <Text>{item.description}</Text>
           <Text>{item.language}</Text>
         </View>
       </View>
       <View style={styles.cardBottom}>
-        <View>
-          <Text>{item.stargazersCount}</Text>
-          <Text>Stars</Text>
-        </View>
-        <View>
-          <Text>{item.forksCount}</Text>
-          <Text>Forks</Text>
-        </View>
-        <View>
-          <Text>{item.reviewCount}</Text>
-          <Text>Reviews</Text>
-        </View>
-        <View>
-          <Text>{item.ratingAverage}</Text>
-          <Text>Rating</Text>
-        </View>
+        <Statistic what="Stars" count={item.stargazersCount} />
+        <Statistic what="Forks" count={item.forksCount} />
+        <Statistic what="Reviews" count={item.reviewCount} />
+        <Statistic what="Rating" count={item.ratingAverage} />
       </View>
     </View>
   );
