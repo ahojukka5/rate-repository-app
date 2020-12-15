4759d015ffee996e7d510bdacea1c7335449ae3d
Jukka Aho <ahojukka5@gmail.com>
2020-12-16 02:02:41 +0700

    Move useRepositories to own file

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+
+import { apiBaseUrl } from '../constants';
+
+const useRepositories = () => {
+  const [repositories, setRepositories] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const fetchRepositories = async () => {
+    setLoading(true);
+
+    const uri = `${apiBaseUrl}/repositories`;
+    const response = await fetch(uri);
+    const json = await response.json();
+
+    setLoading(false);
+    setRepositories(json);
+  };
+
+  useEffect(() => {
+    fetchRepositories();
+  }, []);
+
+  return { repositories, loading, refetch: fetchRepositories };
+};
+
+export default useRepositories;
