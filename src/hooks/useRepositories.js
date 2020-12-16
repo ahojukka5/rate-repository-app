5310e79fe00a12e91cba1791736873c6cd79ad65
Jukka Aho <ahojukka5@gmail.com>
2020-12-17 02:01:01 +0700

    Update useRepositories to use GraphQL

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,27 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/react-hooks';
 
-import config from '../constants';
+import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = () => {
-  const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
-
-  const fetchRepositories = async () => {
-    setLoading(true);
-
-    const uri = `${config.apiBaseUrl}/repositories`;
-    const response = await fetch(uri);
-    const json = await response.json();
-
-    setLoading(false);
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-
-  return { repositories, loading, refetch: fetchRepositories };
+  const { data, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  });
+  return { loading, repositories: loading ? null : data.repositories };
 };
 
 export default useRepositories;
