3188086ede66372f711617fabd24128436c3ec54
Jukka Aho <ahojukka5@gmail.com>
2020-12-17 01:35:02 +0700

    Query GET_REPOSITORIES

---
+++ src/graphql/queries.js
@@ -0,0 +1,13 @@
+import { gql } from 'apollo-boost';
+
+export const GET_REPOSITORIES = gql`
+  query {
+    repositories {
+      edges {
+        node {
+          name
+        }
+      }
+    }
+  }
+`;
