5e74ffda7fe99ed901df5079c85db13fa1008cb8
Jukka Aho <ahojukka5@gmail.com>
2020-12-17 01:18:32 +0700

    Use graphqlUrl when connect ApolloClient

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -2,7 +2,7 @@ import ApolloClient from 'apollo-boost';
 import config from '../constants';
 
 const createApolloClient = () => {
-  return new ApolloClient({ uri: config.apiBaseUrl });
+  return new ApolloClient({ uri: config.graphqlUrl });
 };
 
 export default createApolloClient;
