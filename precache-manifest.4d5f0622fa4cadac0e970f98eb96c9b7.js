b030e47b87e61314e05e97663d2d7f3714f34314
ahojukka5 <ahojukka5@users.noreply.github.com>
2020-12-27 08:43:18 +0000

    deploy: 1acd524716a01c6662b857b3121258ebecd85ee5

---
+++ precache-manifest.4d5f0622fa4cadac0e970f98eb96c9b7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "449eaccfdfe3dbe88a1f",
+    "url": "/rate-repository-app/static/js/app.d4b88a11.chunk.js"
+  },
+  {
+    "revision": "82cba4ad8e42b1a07e2b",
+    "url": "/rate-repository-app/static/js/runtime~app.449fedf4.js"
+  },
+  {
+    "revision": "52d154f99a94f441ce45",
+    "url": "/rate-repository-app/static/js/2.c4dc39ce.chunk.js"
+  },
+  {
+    "revision": "c2502f4483d5184caba20b0607768a4e",
+    "url": "/rate-repository-app/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/rate-repository-app/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/rate-repository-app/expo-service-worker.js"
+  },
+  {
+    "revision": "a912e56e2774943543ed4a60c2f77150",
+    "url": "/rate-repository-app/register-service-worker.js"
+  },
+  {
+    "revision": "3d71af97b6e208e4e680123e701e5c8c",
+    "url": "/rate-repository-app/static/js/2.c4dc39ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/rate-repository-app/favicon-16.png"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/rate-repository-app/favicon-32.png"
+  },
+  {
+    "revision": "50b1bd72d8b0c569ffcad83d622ec176",
+    "url": "/rate-repository-app/favicon.ico"
+  },
+  {
+    "revision": "70658371387475d4f4008c91d8de3737",
+    "url": "/rate-repository-app/manifest.json"
+  }
+];
